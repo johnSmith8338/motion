751e5d527994b87de51a415da2ccec44ad20ebea
Zaporozhan Dmitriy <zaporozhan83@gmail.com>
2018-01-16 18:42:15 +0200

    added updates of tablet mode

--- markup/app/js/main.js
+++ markup/app/js/main.js
@@ -1,11 +1,61 @@
-  $(document).ready(function () {
-      $(".nav-list a[href*=#]").on("click", function (e) {
-          var anchor = $(this);
-          $(".nav-list li").removeClass('active');
-          anchor.parent().addClass('active');
-          $('html, body').stop().animate({
-              scrollTop: $(anchor.attr('href')).offset().top
-          }, 777);
-          e.preventDefault();
-      });
-  });
+$(document).ready(function () {
+    anchorLinks();
+});
+
+// Navigation anchor links
+function anchorLinks() {
+    $(".nav-list a").on("click", function (e) {
+        var anchor = $(this);
+        $(".nav-list li").removeClass('active');
+        anchor.parent().addClass('active');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+    });
+}
+
+// Portfolio slider
+$(".portfolio-list").slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<div class="portfolio-prev-arrow"><div class="chevron-left"></div></div>',
+    nextArrow: '<div class="portfolio-next-arrow"><div class="chevron-right"></div></div>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 479,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+// tweets slider
+$(".tweets-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    draggable: false,
+    prevArrow: '<div class="tweets-prev-arrow"><div class="chevron-left"></div></div>',
+    nextArrow: '<div class="tweets-next-arrow"><div class="chevron-right"></div></div>'
+});
